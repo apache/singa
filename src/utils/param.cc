d5b6a30d0efd207da143d01ca4079f2caa2ba5f7
Wei Wang <wangwei@comp.nus.edu.sg>
2015-08-12 15:29:37 +0800

    SINGA-49 Fix a bug in HandlePutMsg func that sets param fields to invalid values
    The Param::HandlePutMsg doesn't set the weight decay multiplier and learning rate multiplier correctly.
    The previous code doesn't use the values from the received messages.
    Fixed the bug using the correct multipliers.

--- src/utils/param.cc
+++ src/utils/param.cc
@@ -18,7 +18,7 @@ void Param::Setup(const ParamProto& proto, const vector<int>& shape){
   data_=std::make_shared<Blob<float>>(shape);
   grad_.Reshape(shape);
   history_.Reshape(shape);
-  proto_=proto;
+  proto_.CopyFrom(proto);
 }
 
 void Param::AddSlice(int slice_id, int size){
@@ -146,10 +146,10 @@ Msg* Param::HandlePutMsg(Msg** msg, bool reserve) {
   float lr, wc;
   float* ptr;
   (*msg)->ParseFormatFrame("iffp", &size, &lr, &wc, &ptr);
-  proto_.set_learning_rate_multiplier(lr);
-  proto_.set_weight_decay_multiplier(wc);
-  vector<int> shape{size};
   ParamProto proto;
+  proto.set_learning_rate_multiplier(lr);
+  proto.set_weight_decay_multiplier(wc);
+  vector<int> shape{size};
   Setup(proto, shape);
   if (ptr == nullptr) {
     CHECK((*msg)->NextFrame());
