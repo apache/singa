748664780cb612d5e9324e4f3b45560e20b424be
jinyangturbo <pku.turbo@gmail.com>
2015-09-10 00:37:01 -0700

    SINGA-68 Preparation for Release -- License etc.
    Added ASF License to all source files.
    Removed gflags from LICENSE
    Prepared NOTICE and DISCLAIMER file.

--- src/utils/common.cc
+++ src/utils/common.cc
@@ -1,3 +1,24 @@
+/************************************************************
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*
+*************************************************************/
+
 /**
  * The some functions in this file are adapted from Caffe whose license
  * is attached.
@@ -38,6 +59,7 @@
  * or otherwise, the contributor releases their content to the
  * license and copyright terms herein.
  */
+
 #include "utils/common.h"
 
 #include <sys/ioctl.h>
@@ -93,8 +115,6 @@ string StringPrintf(string fmt, ...) {
   va_end(l);
   return result;
 }
-
-
 
 int ArgPos(int argc, char** arglist, const char* arg) {
   for (int i = 0; i < argc; i++) {
