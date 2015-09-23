6a39380dd61403695efd3a0ec11a6832980ebe1c
Wei Wang <wangwei@comp.nus.edu.sg>
2015-09-23 21:24:05 +0800

    SINGA-72 Minor updates to be consisten with documentation
    * Update driver.cc to remove the dependency on OpenBLAS's cblas.h file due to the use of openblas_set_num_threads().
    * Update README.md file to add comments and instructions for users to
    install and try one example.

--- src/driver.cc
+++ src/driver.cc
@@ -7,9 +7,9 @@
 * to you under the Apache License, Version 2.0 (the
 * "License"); you may not use this file except in compliance
 * with the License.  You may obtain a copy of the License at
-* 
+*
 *   http://www.apache.org/licenses/LICENSE-2.0
-* 
+*
 * Unless required by applicable law or agreed to in writing,
 * software distributed under the License is distributed on an
 * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,10 +21,11 @@
 
 #include "driver.h"
 
-#include <cblas.h>
 #include <glog/logging.h>
 #include <string>
 #include "utils/tinydir.h"
+
+extern "C" void openblas_set_num_threads(int);
 
 namespace singa {
 
