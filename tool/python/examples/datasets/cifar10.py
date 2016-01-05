4ebf0bcd6165935079407564368e901b429326b5
Wei Wang <wangwei@comp.nus.edu.sg>
2016-01-06 02:37:17 +0800

    Add license for python files to pass 'make rat'

--- tool/python/examples/datasets/cifar10.py
+++ tool/python/examples/datasets/cifar10.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
+
+#/************************************************************
+#*
+#* Licensed to the Apache Software Foundation (ASF) under one
+#* or more contributor license agreements.  See the NOTICE file
+#* distributed with this work for additional information
+#* regarding copyright ownership.  The ASF licenses this file
+#* to you under the Apache License, Version 2.0 (the
+#* "License"); you may not use this file except in compliance
+#* with the License.  You may obtain a copy of the License at
+#*
+#*   http://www.apache.org/licenses/LICENSE-2.0
+#*
+#* Unless required by applicable law or agreed to in writing,
+#* software distributed under the License is distributed on an
+#* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#* KIND, either express or implied.  See the License for the
+#* specific language governing permissions and limitations
+#* under the License.
+#*
+#*************************************************************/
+
+
 from singa.model import *
 
 def load_data(
-         workspace = None, 
+         workspace = None,
          backend = 'kvfile',
          batchsize = 64,
          random = 5000,
@@ -20,13 +43,13 @@ def load_data(
 
   store = Store(path=path_train, mean_file=path_mean, backend=backend,
               random_skip=random, batchsize=batchsize,
-              shape=shape) 
+              shape=shape)
 
   data_train = Data(load='recordinput', phase='train', conf=store)
 
   store = Store(path=path_test, mean_file=path_mean, backend=backend,
               batchsize=batchsize,
-              shape=shape) 
+              shape=shape)
 
   data_test = Data(load='recordinput', phase='test', conf=store)
 
--- tool/python/examples/mnist_rbm1.py
+++ tool/python/examples/mnist_rbm1.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
+#/************************************************************
+#*
+#* Licensed to the Apache Software Foundation (ASF) under one
+#* or more contributor license agreements.  See the NOTICE file
+#* distributed with this work for additional information
+#* regarding copyright ownership.  The ASF licenses this file
+#* to you under the Apache License, Version 2.0 (the
+#* "License"); you may not use this file except in compliance
+#* with the License.  You may obtain a copy of the License at
+#*
+#*   http://www.apache.org/licenses/LICENSE-2.0
+#*
+#* Unless required by applicable law or agreed to in writing,
+#* software distributed under the License is distributed on an
+#* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#* KIND, either express or implied.  See the License for the
+#* specific language governing permissions and limitations
+#* under the License.
+#*
+#*************************************************************/
+
+
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__),'..')) 
+sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 from singa.model import *
 from examples.datasets import mnist
 
@@ -14,7 +36,7 @@ X_train, X_test, workspace = mnist.load_data(
 
 m = Energy('rbm'+str(rbmid), sys.argv)
 
-m.add(RBM(1000, w_std=0.1, b_wd=0)) 
+m.add(RBM(1000, w_std=0.1, b_wd=0))
 
 sgd = SGD(lr=0.1, decay=0.0002, momentum=0.8)
 topo = Cluster(workspace)
