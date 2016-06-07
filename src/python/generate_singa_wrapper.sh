2ff9bb370bc3df20a0db39d2befb015d1d426870
chonho <leech@comp.nus.edu.sg>
2016-06-08 01:03:05 +0800

    SINGA-186 Create Python base layer
    - Add Swig interfaces: singa.i, core_tensor.i
    - Add example_tensor.py
    - Add generate_singa_wrapper.sh to generate wrapper

---
+++ src/python/generate_singa_wrapper.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#/**
+# * Licensed to the Apache Software Foundation (ASF) under one
+# * or more contributor license agreements.  See the NOTICE file
+# * distributed with this work for additional information
+# * regarding copyright ownership.  The ASF licenses this file
+# * to you under the Apache License, Version 2.0 (the
+# * "License"); you may not use this file except in compliance
+# * with the License.  You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+SINGA_ROOT=/home/chonho/incubator-singa
+SINGA_SRC=${SINGA_ROOT}/src
+SRC_CC=(${SINGA_SRC}/core/tensor/tensor.cc \
+        ${SINGA_SRC}/core/device/device.cc
+       )
+USR_LOCAL=/home/chonho/local
+
+#The following commands are only for developers adding new py apis.
+#swig -c++ -python -w509 -I../../include singa.i
+swig -c++ -python -I../../include singa.i
+
+g++ -fPIC ${SRC_CC[@]} singa_wrap.cxx -shared -o _singa.so \
+    -L${USR_LOCAL}/lib -lprotobuf -Wl,-rpath=${USR_LOCAL}/lib \
+    -L../../lib -lsinga_core -lsinga_model -lsinga_utils -Wl,-rpath=../../lib \
+    -std=c++11 \
+    -I../.. \
+    -I../../include \
+    -I${SINGA_SRC} \
+    -I${USR_LOCAL}/include \
+    -I${USR_LOCAL}/cudnn/include \
+    -I/usr/include/python2.7 \
+    -I/usr/local/cuda-7.0/include
+
+#python example.py
