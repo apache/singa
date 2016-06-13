a2a8e34b4c33be406af081c731d7ee990936701f
WANG Sheng <wangsheng1001@gmail.com>
2016-06-13 11:20:43 +0800

    SINGA-195 Channel for sending training statistics
    Add utils/channel.h, utils/channel.cc, test/test_channel.cc
    
    Channel provides functionalities of unifily sending training statistics
    to different terminals.
    
    Upon setup, config it in global:
    InitChannel();
    SetChannelDirectory(path);
    
    To get channel instance:
    GetChannel(channel_name);
    
    Then send messages:
    channel->Send(message);

---
+++ include/singa/utils/channel.h
@@ -0,0 +1,76 @@
+/************************************************************
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*
+*************************************************************/
+
+#ifndef SINGA_UTILS_CHANNEL_H_
+#define SINGA_UTILS_CHANNEL_H_
+
+#include <google/protobuf/message.h>
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+namespace singa {
+
+class Channel {
+ public:
+  explicit Channel(const std::string& name);
+  ~Channel();
+
+  inline const std::string& GetName() { return name_; }
+  inline void EnableDestStderr(bool enable) { stderr_ = enable; }
+  inline void EnableDestFile(bool enable) { file_ = enable; }
+  void SetDestFilePath(const std::string& file);
+  void Send(const std::string& message);
+  void Send(const google::protobuf::Message& message);
+
+ private:
+  std::string name_ = "";
+  bool stderr_ = false;
+  bool file_ = false;
+  std::ofstream os_;
+};
+
+class ChannelManager {
+ public:
+  ChannelManager() {}
+  ~ChannelManager();
+
+  void Init();
+  void SetDefaultDir(const char* dir);
+  Channel* GetInstance(const std::string& channel);
+
+ private:
+  std::string dir_ = "";
+  std::map<std::string, Channel*> name2ptr_;
+};
+
+/// Initial function for global usage of channel
+void InitChannel(const char* argv);
+/// Set the directory name for persisting channel content
+void SetChannelDirectory(const char* path);
+/// Get the channel instance
+Channel* GetChannel(const std::string& channel_name);
+
+}  // namespace singa
+
+#endif  // SINGA_UTILS_CHANNEL_H__
