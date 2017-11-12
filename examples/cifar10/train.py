7129fd0eac8ea393f5fb545ca4662f5ec3123c04
junzhezhang <zhangjunzhetom@gmail.com>
2017-11-12 14:42:03 +0800

    customize cifar10 train.py again

--- examples/cifar10/train.py
+++ examples/cifar10/train.py
@@ -153,7 +153,7 @@ def train(data, net, max_epoch, get_lr, weight_decay, batch_size=100,
         fileTimeLog.write('Epoch %d: ' % epoch)
         fileTimeLog.write(str(int(round(time.time()*1000))))
         fileTimeLog.write('\n')
-	    for b in range(1):
+        for b in range(1):
             time.sleep(0.5)
             fileTimeLog.write('iteration %d: ' % b)
             fileTimeLog.write(str(int(round(time.time()*1000))))
