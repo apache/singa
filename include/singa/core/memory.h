b2a7f55b9dda0eb819a40b316f248013a3518618
junzhezhang <30921847+junzhezhang@users.noreply.github.com>
2017-12-14 11:33:23 +0800

    Update memory.h
    update memory.h

--- include/singa/core/memory.h
+++ include/singa/core/memory.h
@@ -23,6 +23,17 @@
 #include <atomic>
 #include "singa/proto/core.pb.h"
 #include "singa/singa_config.h"
+//for SmartMemPool
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <stdlib.h>     /* malloc, free, rand */
+#include <map>
+using namespace std;
 
 #ifdef USE_CUDA
 #include "cnmem.h"
@@ -86,6 +97,50 @@ class CudaMemPool : public DeviceMemPool {
   void Malloc(void** ptr, const size_t size) override;
   void Free(void* ptr) override;
 };
+
+//for SmartMemPool
+struct lookUpElement{
+    /*
+     for memory pool Malloc look-up table.
+     */
+    int r_idx;
+    int d_idx;
+    size_t size;
+    size_t offset;
+    void* ptr;
+    int Occupied; //0 is free, 1 is occupied.
+};
+
+///class mem-pool SmartMemPool
+class SmartMemPool{
+public:
+    SmartMemPool(string meth); //constructor
+    //TODO(junzhe) in Singa, void Malloc( void**, size_t); change to cudaMalloc and cudaFree.
+    void Malloc(void** ptr, size_t size);
+    void Free(void* ptr);
+    ~SmartMemPool();
+    void getMaxLoad(void);
+private:
+    string colorMethod;
+    int mallocFlag =0; //0 for cudaMalloc, 1 for coloringMalloc
+    int gc =0; //global counter each time Malloc/Free, add 1.
+    int globeCounter=-1;
+    int loadLogFlag =1; //record when its 1.
+    void* ptrPool = NULL;
+    int idxRange = 0;
+    size_t offset = 0;
+    int maxLen =0;
+    int location=0;
+    vector<string> vec;
+    map<int,int>Table_r2d; //full duration info, cross-iteration duration.
+    map<int,int>Table_d2r;
+    map<int,lookUpElement>Table_r2Ver;
+    map<int, pair<size_t,size_t>>Table_load; //gc, <cudaLoad, colorLoad>
+    map<void*,size_t>Table_p2s; //For tracking load in Free. add when allocate, delete when deallocate.
+    map<void*,int>Table_p2r; //ptr for arrival idx, for look up Table during free
+    int checkPoint=300; //for reduce number of test.
+};
+
 #endif
 }  // namespace singa
 #endif  // SINGA_CORE_MEMORY_H_
