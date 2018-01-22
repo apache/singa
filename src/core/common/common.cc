25d2a88ed364b643c79e9df02468c2905e22461d
junzhezhang <zhangjunzhetom@gmail.com>
2018-01-22 22:31:39 +0800

    add swap class, packaging with unique ptr for each block

--- src/core/common/common.cc
+++ src/core/common/common.cc
@@ -40,7 +40,7 @@ void* Block::mutable_data() {
         ptrDevice_->AppendInfo(temp);
     }
     void* realPtr_ = ptrDevice_->GetRealGpuPtrInfo(data_);
-    ptrDevice_->SwapOutInfo(data_);
+    //ptrDevice_->SwapOutInfo(data_);
 
     return static_cast<char*>(realPtr_) + offset_;
   }
@@ -60,7 +60,7 @@ const void* Block::data() const {
     }
 
     void* realPtr_ = ptrDevice_->GetRealGpuPtrInfo(data_);
-    ptrDevice_->SwapOutInfo(data_);
+    //ptrDevice_->SwapOutInfo(data_);
     return static_cast<char*>(realPtr_) + offset_;
   }
 
