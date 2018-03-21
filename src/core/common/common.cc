5b5d460cbb7d3045642b59b46b3a8df4a7d0437f
junzhezhang <zhangjunzhetom@gmail.com>
2018-03-21 14:37:58 +0800

    swapIn

--- src/core/common/common.cc
+++ src/core/common/common.cc
@@ -28,6 +28,7 @@
 namespace singa {
 
 void* Block::mutable_data() {
+    std::cout<<"data"<<std::endl;
     initialized_ = true;
     if (ptrDevice_!=nullptr){
         stringstream strm2;
@@ -50,6 +51,7 @@ void* Block::mutable_data() {
 
 const void* Block::data() const {
     CHECK(initialized_) << "Must initialize data before reading it";
+    std::cout<<"data"<<std::endl;
     if (ptrDevice_!=nullptr){
         stringstream strm2;
         strm2<<this;
