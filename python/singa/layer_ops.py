6402a53d31185bb455c9d796b03d01f3dc476de3
xuewanqi <36396136+xuewanqi@users.noreply.github.com>
2018-05-05 17:00:22 +0800

    SINGA-349 Create layer operations for autograd
    1. Change the API of Conv2d operations into Pytorch style. next step is to confirm the new design workable.
    
    2. Add flags in Conv2d forward function
    
    3. Delete extra file

--- python/singa/layer_ops.py
+++ python/singa/layer_ops.py
@@ -1,26 +1,36 @@
 from singa import tensor
 from singa import layer
 from singa.proto import model_pb2
-from singa import autograd
 
 
+class Conv2D(tensor.Operation):
+    def __init__(self,in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True,**kwargs):
 
-def ctensor2numpy(x):
-    '''
-    // For test use.
+        name='Conv2d'
+        border_mode = 'same'
+        cudnn_prefer = 'fastest'
+        workspace_byte_limit = 1024
+        data_format = 'NCHW'
+        W_specs = None
+        b_specs = None
+        input_sample_shape=None
 
+        allowed_kwargs = {'name':name,
+                          'border_mode':border_mode,
+                          'cudnn_prefer':cudnn_prefer,
+                          'workspace_byte_limit':workspace_byte_limit,
+                          'data_format':data_format,
+                          'W_specs':W_specs,
+                          'b_specs':b_specs,
+                          'input_sample_shape':input_sample_shape
+                          }
 
-    To be used in SoftMax Operation.
-    Convert a singa_tensor to numpy_tensor.
-    '''
-    np_array = x.GetFloatValue(int(x.Size()))
-    return np_array.reshape(x.shape())
+        for kwarg in kwargs:
+            if kwarg not in allowed_kwargs:
+                raise TypeError('Keyword argument not understood:', kwarg)
+            else:
+                allowed_kwargs[kwarg] = kwargs[kwarg]
 
-class Convolution2D(tensor.Operation):
-    def __init__(self, name, nb_kernels, kernel=3, stride=1, border_mode='same',
-                 cudnn_prefer='fastest', workspace_byte_limit=1024,
-                 data_format='NCHW', use_bias=True, W_specs=None, b_specs=None,
-                 pad=None,input_sample_shape=None):
         '''
         How to match Keras:
 
@@ -29,10 +39,10 @@ class Convolution2D(tensor.Operation):
         shape -> kernel+input_sample_shape[0](nb_channels)+nb_kernels,
         initializer, name, regularizer, constraint -> W_specs.
         '''
-        self.PyLayer = layer.Conv2D(name, nb_kernels, kernel=kernel, stride=stride, border_mode=border_mode,
+        self.PyLayer = layer.Conv2D(name, nb_kernels=out_channels, kernel=kernel_size, stride=stride, border_mode=border_mode,
                  cudnn_prefer=cudnn_prefer, workspace_byte_limit=workspace_byte_limit,
-                 data_format=data_format, use_bias=use_bias, W_specs=W_specs, b_specs=b_specs,
-                 pad=pad, input_sample_shape=input_sample_shape)
+                 data_format=data_format, use_bias=bias, W_specs=W_specs, b_specs=b_specs,
+                 pad=padding, input_sample_shape=input_sample_shape)
 
 
     def __call__(self, x):
@@ -53,8 +63,11 @@ class Convolution2D(tensor.Operation):
         xs = tuple(xs)
         return self._do_forward(*xs)
 
-    def forward(self, *xs):
-        return self.PyLayer.layer.Forward(4, xs[0])  #how ktrain works?  flag & ktrain.
+    def forward(self, flag=True,*xs):
+        if flag is True:
+            return self.PyLayer.layer.Forward(4, xs[0])
+        else:
+            return self.PyLayer.layer.Forward(8, xs[0])
 
     def backward(self, dy):
         ret = self.PyLayer.layer.Backward(0, dy)
@@ -78,7 +91,7 @@ class MaxPooling2D(tensor.Operation):
         return self.PyLayer.layer.Forward(4, x)
 
     def backward(self, dy):
-        return self.PyLayer.layer.Backward(0, dy)[0]   # how backward() return?
+        return self.PyLayer.layer.Backward(0, dy)[0]
 
 
 class Activation(tensor.Operation):
