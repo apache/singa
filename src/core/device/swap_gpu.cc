28bacc549bd7f2f24a0407ed566945702d02aefa
junzhezhang <zhangjunzhetom@gmail.com>
2018-05-05 23:32:58 +0800

    update Table_Block_

--- src/core/device/swap_gpu.cc
+++ src/core/device/swap_gpu.cc
@@ -808,7 +808,7 @@ void SwapGPU::MakeMetaTable(Block* block_,void* data_,int size){
 
   //update Table_Block_, to reduce time complexity TODO(junzhe)
   if (!(Table_Block_.find((gc-location)%maxLen)==Table_Block_.end())){
-      Table_Block_.find((gc-location)%maxLen) = block_;
+      Table_Block_.at((gc-location)%maxLen) = block_;
   }
 
 }
