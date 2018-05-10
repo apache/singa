8ad98747ac03488872b9d97e7aa950ad0635a344
junzhezhang <zhangjunzhetom@gmail.com>
2018-05-10 11:09:46 +0800

    manipulate event signal

--- src/core/device/swap_gpu.cc
+++ src/core/device/swap_gpu.cc
@@ -592,7 +592,7 @@ int SwapGPU::swap_test(vector<string>vec_block,int &maxLen, int &location){
         }
         Table_meta_ridx[vec_swap_selct[i].r_idx] = Table_meta.find(static_cast<Block*>(tempBlock_))->second;
 
-        cout<<"complete store: "<<vec_swap_selct[i].i1<<' '<<vec_swap_selct[i].i2p<<endl;
+        cout<<"complete store: "<<vec_swap_selct[i].i1<<' '<<vec_swap_selct[i].i2p<<' '<<Table_meta.find(static_cast<Block*>(tempBlock_))->second.second.size;<<endl;
     }
 
   //update globeCounter
