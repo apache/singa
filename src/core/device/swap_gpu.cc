8d3b6cf31205d8b9d0d7b3e6861131e7bb636dab
junzhezhang <zhangjunzhetom@gmail.com>
2018-05-29 21:00:02 +0800

    add global_load

--- src/core/device/swap_gpu.cc
+++ src/core/device/swap_gpu.cc
@@ -430,7 +430,7 @@ int SwapGPU::swap_test(vector<string>vec_block,int &maxLen, int &location){
   cout<<"============== select top a few to swap"<<endl;
   cout<<"maxIdx and maxLoad are: "<<maxIdx<<' '<<maxLoad<<endl;
   cout<<"sumSizeSwapAble: "<<sumSizeSwapAble<<endl;
-  size_t memLimit = 10000000; //changed from 20 to 15, 10
+  size_t memLimit = 170000000; //changed from 20 to 15, 10
   sort(vec_swap.begin(),vec_swap.end(),less_than_pri());
   vector<onePairMsg_Swap>vec_swap_selct;
   size_t sumSizeToSwap=0;
