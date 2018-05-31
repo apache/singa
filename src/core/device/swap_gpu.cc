291576b0bf96bf1dbfe3eefe219f157b2156692e
junzhezhang <zhangjunzhetom@gmail.com>
2018-05-31 22:38:54 +0800

    double swap speed

--- src/core/device/swap_gpu.cc
+++ src/core/device/swap_gpu.cc
@@ -228,12 +228,12 @@ struct less_than_Idx{
 int SwapInTime(size_t size){
     //yet to get the formula
     int ans =0; //TODO(junzhe) used to be 0.13 --> 0.26 (unpinned vs pinned)
-    if (size==0) {ans = 9500;} else {ans = 0.13*size;}
+    if (size==0) {ans = 9500;} else {ans = 0.26*size;}
     return ans;
 }
 int SwapOutTime(size_t size){
     int ans =0; //TODO(junzhe) used to be 0.29 --> 0.58
-    if (size==0) {ans = 17000;} else {ans = 0.29*size;}
+    if (size==0) {ans = 17000;} else {ans = 0.58*size;}
     return ans;
 }
 
