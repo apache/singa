4cd9f3d7bdd6187c8a5b86fe2ab97cb05ddb83cf
junzhezhang <zhangjunzhetom@gmail.com>
2018-06-10 14:29:45 +0800

    stick-to-limit, with 500 MB

--- src/core/device/swap_gpu.cc
+++ src/core/device/swap_gpu.cc
@@ -677,13 +677,13 @@ void SwapGPU::swap_plan(){
   auto vec_load_wdto = origin_load;
 
   double overhead = 0;
-  swap_sched(vec_swap_pri, vec_load_pri,overhead,0,"stick-to-limit");
+  swap_sched(vec_swap_pri, vec_load_pri,overhead,500<<20,"stick-to-limit");
   cout<<"load 2 overhead pri: "<<overhead<<endl;
   overhead = 0;
-  swap_sched(vec_swap_dto, vec_load_dto,overhead,0,"stick-to-limit");
+  swap_sched(vec_swap_dto, vec_load_dto,overhead,500<<20,"stick-to-limit");
   cout<<"load 2 overhead dto: "<<overhead<<endl;
   overhead = 0;
-  swap_sched(vec_swap_wdto, vec_load_wdto,overhead,0,"stick-to-limit");
+  swap_sched(vec_swap_wdto, vec_load_wdto,overhead,500<<20,"stick-to-limit");
   cout<<"load 2 overhead wdto: "<<overhead<<endl;
 
 
