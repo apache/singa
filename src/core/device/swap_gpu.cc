5d82942743f72c2f0c083df5e52c00a94d86f9b1
junzhezhang <zhangjunzhetom@gmail.com>
2018-06-12 19:28:44 +0800

    result: no overhead

--- src/core/device/swap_gpu.cc
+++ src/core/device/swap_gpu.cc
@@ -702,15 +702,15 @@ void SwapGPU::swap_plan(){
   auto vec_load_pri = origin_load;
   auto vec_load_dto = origin_load;
   auto vec_load_wdto = origin_load;
-  string mode = "stick-to-limit";
+  string mode = "no-overhead";//"stick-to-limit";
   double overhead = 0;
-  swap_sched(vec_swap_pri, vec_load_pri,overhead,400<<20, mode);
+  swap_sched(vec_swap_pri, vec_load_pri,overhead,550<<20, mode);
   cout<<"load 2 overhead pri: "<<overhead<<endl;
   overhead = 0;
-  swap_sched(vec_swap_dto, vec_load_dto,overhead,400<<20,mode);
+  swap_sched(vec_swap_dto, vec_load_dto,overhead,550<<20,mode);
   cout<<"load 2 overhead dto: "<<overhead<<endl;
   overhead = 0;
-  swap_sched(vec_swap_wdto, vec_load_wdto,overhead,400<<20,mode);
+  swap_sched(vec_swap_wdto, vec_load_wdto,overhead,550<<20,mode);
   cout<<"load 2 overhead wdto: "<<overhead<<endl;
 
 
