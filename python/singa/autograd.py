d5422a432d1ad4261f3b44b1f61af5f4c2a651ec
xuewanqi <xue_wanqi@outlook.com>
2018-08-21 14:17:00 +0000

    SINGA-383 Add Separable Convolution for autograd
    - Implement Xception net by calling SeparableConv2d layer. The file is added to   /example/autograd folder.
    
    - Modified SeparableConv2d layer API.

--- python/singa/autograd.py
+++ python/singa/autograd.py
@@ -779,12 +779,12 @@ class Conv2d(Layer):
 
 class SeparableConv2d(Layer):
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=False):
 
         self.mapping_spacial_conv = Conv2d(
-            in_channels, in_channels, kernel_size, stride, padding, groups=in_channels, bias=False)
+            in_channels, in_channels, kernel_size, stride, padding, groups=in_channels, bias=bias)
 
-        self.mapping_depth_conv = Conv2d(in_channels, out_channels, 1, bias=False)
+        self.mapping_depth_conv = Conv2d(in_channels, out_channels, 1, bias=bias)
 
     def __call__(self, x):
         y = self.mapping_spacial_conv(x)
