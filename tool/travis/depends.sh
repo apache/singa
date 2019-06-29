6d4a631828e14f441e667bcd1a7188e14c0712d7
Wang Wei <wangwei.cs@gmail.com>
2019-06-29 23:00:43 +0800

    SINGA-467 Update the version of singa-gpu (singa-cpu) package and fix travis build error
    1. Debug travis error when building singa on osx.
       I tried various configuration of the osx_image version.
       The solution is a) osx_image: xcode8 (cmake will use the system cblas
       instead of the one installed by conda build when using the default
       osx image); b) download sdk10.9 manually and
       let cmake use it
       (https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html).
       otherwise, cmake will fail to compile simple test programs.
    
    2. update singa-cpu and singa-gpu to depend on singa 2.0.0
    
    3. fix the test section in meta.yaml to do test after conda building.

--- tool/travis/depends.sh
+++ tool/travis/depends.sh
@@ -21,6 +21,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]];
 then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 else
+  # https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#macos-sdk
+  wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
+  tar xf MacOSX10.9.sdk.tar.xz -C /tmp/
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
 fi
 bash miniconda.sh -b -p $HOME/miniconda
