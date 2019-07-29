c46d347da4c8c8f1e09b4e2b03254d29e05d5c87
pinpom <thao.nguyen038@gmail.com>
2019-07-29 09:32:56 +0800

    add Sub operator

--- test/python/test_operation.py
+++ test/python/test_operation.py
@@ -322,6 +322,21 @@ class TestPythonOperation(unittest.TestCase):
         np.testing.assert_array_almost_equal(tensor.to_numpy(result), XT)
         self.check_shape(dx.shape(), (3, 2))
 
+    def test_Sub(self):
+        X=np.array([0.8,-1.2,3.3,-3.6,-0.5,0.5]).reshape(3,2).astype(np.float32)
+        Y=np.array([4.4,5.3,3.2,3.7,5.4,6.3]).reshape(3,2).astype(np.float32)
+        x = tensor.from_numpy(X)
+        y = tensor.from_numpy(Y)
+        x.to_device(gpu_dev) 
+        y.to_device(gpu_dev) 
+
+        result=autograd.sub(x,y)
+        dx=result.creator.backward(x.data)[0]  
+
+        result_np=np.subtract(X,Y)      
+        np.testing.assert_array_almost_equal(tensor.to_numpy(result), result_np)
+        self.check_shape(dx.shape(), (3, 2)) 
+
 
 if __name__ == '__main__':
     unittest.main()
