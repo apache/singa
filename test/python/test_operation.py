8922739734af88a803d1846098c41b5155b885a1
ShichengChen <c337134154@gmail.com>
2019-07-31 20:27:18 +0800

    SINGA-474 elu operator, modified backward test

--- test/python/test_operation.py
+++ test/python/test_operation.py
@@ -33,6 +33,20 @@ CTensor = singa.Tensor
 dy = CTensor([2, 1, 2, 2])
 singa.Gaussian(0.0, 1.0, dy)
 
+def eval_numerical_gradient(f, x):
+    h = 0.00001
+    fx = f(x) # evaluate function value at original point
+    grad = np.zeros(x.shape)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
+        old_value = x[ix]
+        x[ix] = old_value + h # increment by h
+        fxh = f(x) # evalute f(x + h)
+        x[ix] = old_value # restore to previous value (very important!)
+        grad[ix] = (fxh - fx) / h # the slope
+        it.iternext() # step to next dimension
+    return grad
 
 def _tuple_to_string(t):
     lt = [str(x) for x in t]
@@ -324,38 +338,32 @@ class TestPythonOperation(unittest.TestCase):
 
     def test_Elu_cpu(self):
         #f(x) = alpha * (exp(x) - 1.) for x < 0, f(x) = x for x >= 0
-        x = np.array([-0.9, -0.3, -0.1, 0.1, 0.5, 0.9]).reshape(3, 2).astype(np.float32)
-        h=(1e-5)
-        x1 = x+h
+        x = np.array([-0.9, -0.3, -0.1, 0.1, 0.5, 0.9]).reshape(3, 2).astype(np.float32)/10
+        lossf = lambda x : np.sum(np.clip(x, 0, np.inf) + (np.exp(np.clip(x, -np.inf, 0)) - 1) * 1.0)
         y = np.clip(x, 0, np.inf) + (np.exp(np.clip(x, -np.inf, 0)) - 1) * 1.0
-        y1 = np.clip(x1, 0, np.inf) + (np.exp(np.clip(x1, -np.inf, 0)) - 1) * 1.0
+        grad = eval_numerical_gradient(lossf,x)
         x = tensor.from_numpy(x)
         x.to_device(cpu_dev)
-
         result = autograd.elu(x)
         dy = tensor.from_numpy(np.ones((3,2)).astype(np.float32))
         dy.to_device(cpu_dev)
         dx = result.creator.backward(dy.data)
-
         np.testing.assert_array_almost_equal(tensor.to_numpy(result), y, decimal=5)
-        np.testing.assert_array_almost_equal(tensor.to_numpy(tensor.from_raw_tensor(dx)), (y1-y)/h, decimal=2)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(tensor.from_raw_tensor(dx)), grad, decimal=2)
     def test_Elu_gpu(self):
         #f(x) = alpha * (exp(x) - 1.) for x < 0, f(x) = x for x >= 0
-        x = np.array([-0.9, -0.3, -0.1, 0.1, 0.5, 0.9]).reshape(3, 2).astype(np.float32)
-        h=(1e-5)
-        x1 = x+h
+        x = np.array([-0.9, -0.3, -0.1, 0.1, 0.5, 0.9]).reshape(3, 2).astype(np.float32)/10
+        lossf = lambda x : np.sum(np.clip(x, 0, np.inf) + (np.exp(np.clip(x, -np.inf, 0)) - 1) * 1.0)
         y = np.clip(x, 0, np.inf) + (np.exp(np.clip(x, -np.inf, 0)) - 1) * 1.0
-        y1 = np.clip(x1, 0, np.inf) + (np.exp(np.clip(x1, -np.inf, 0)) - 1) * 1.0
+        grad = eval_numerical_gradient(lossf,x)
         x = tensor.from_numpy(x)
         x.to_device(gpu_dev)
-
         result = autograd.elu(x)
         dy = tensor.from_numpy(np.ones((3,2)).astype(np.float32))
         dy.to_device(gpu_dev)
         dx = result.creator.backward(dy.data)
-
         np.testing.assert_array_almost_equal(tensor.to_numpy(result), y, decimal=5)
-        np.testing.assert_array_almost_equal(tensor.to_numpy(tensor.from_raw_tensor(dx)), (y1-y)/h, decimal=2)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(tensor.from_raw_tensor(dx)), grad, decimal=2)
 
 if __name__ == '__main__':
     unittest.main()
