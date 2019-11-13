f026456a2569fb135062e8ca4e78bbba5b527a65
dcslin <13751447+dcslin@users.noreply.github.com>
2019-11-13 07:42:52 +0000

    more tests on transpose then tensor mult

--- test/python/test_api.py
+++ test/python/test_api.py
@@ -29,7 +29,6 @@ from cuda_helper import gpu_dev, cpu_dev
 
 
 class TestAPI(unittest.TestCase):
-
     def test_softmax_api(self):
         def _run_test(org_shape, axis, aft_shape):
             x_0 = np.random.random(org_shape).astype(np.float32)
@@ -42,11 +41,13 @@ class TestAPI(unittest.TestCase):
             # test with numpy
             x_0 = x_0.reshape(aft_shape)
             x_0 = x_0 - np.max(x_0)
-            y1 = np.divide(np.exp(x_0), np.sum(np.exp(x_0),axis=1).reshape(x_0.shape[0],1) ) # 2d softmax
+            y1 = np.divide(np.exp(x_0),
+                           np.sum(np.exp(x_0),
+                                  axis=1).reshape(x_0.shape[0],
+                                                  1))  # 2d softmax
             y1 = y1.reshape(org_shape)
 
             np.testing.assert_array_almost_equal(tensor.to_numpy(y0), y1)
-
 
         _run_test([2, 2], 1, [2, 2])
         _run_test([2, 2], 0, [1, 4])
@@ -70,83 +71,110 @@ class TestAPI(unittest.TestCase):
         _run_test([2, 2, 2, 2], -4, [1, 16])
 
     def test_tensor_add_mul_api(self):
-
         def _run_test(singa_op, np_op, s1, s2):
             x_0 = np.random.random(s1).astype(np.float32)
             y_0 = np.random.random(s2).astype(np.float32)
             x0 = tensor.Tensor(device=gpu_dev, data=x_0)
             y0 = tensor.Tensor(device=gpu_dev, data=y_0)
 
-            z0 = tensor._call_singa_func(singa_op, x0.data,y0.data)
+            z0 = tensor._call_singa_func(singa_op, x0.data, y0.data)
             z0.to_host()
             #z0 = x0*y0
 
             #print(s1,s2,tensor.to_numpy(z0).shape)
-            np.testing.assert_array_almost_equal(tensor.to_numpy(z0), np_op(x_0,y_0) )
+            np.testing.assert_array_almost_equal(tensor.to_numpy(z0),
+                                                 np_op(x_0, y_0))
             return
 
-        _run_test(singa_api.__add__,np.add,[6],[1])
-        _run_test(singa_api.__add__,np.add,[3,2],[1])
-        _run_test(singa_api.__add__,np.add,[3,1,2],[3,1,1])
-        _run_test(singa_api.__add__,np.add,[2,3,4,5],[5])
-        _run_test(singa_api.__add__,np.add,[2,3,4,5],[1,1,1])
-        _run_test(singa_api.__add__,np.add,[2,3,4,5],[1,1,1,1])
-        _run_test(singa_api.__add__,np.add,[3,1,2,1],[3,1,2])
-        _run_test(singa_api.__add__,np.add,[4,5],[2,3,4,5]) # 45+2345=2345
-        _run_test(singa_api.__add__,np.add,[2,3,4,5],[4,5]) # 45+2345=2345
-        _run_test(singa_api.__add__,np.add,[1,4,5],[2,3,1,1]) # 145+2311=2345
-        _run_test(singa_api.__add__,np.add,[3,4,5],[2,1,1,1]) # 345+2111=2345
+        _run_test(singa_api.__add__, np.add, [6], [1])
+        _run_test(singa_api.__add__, np.add, [3, 2], [1])
+        _run_test(singa_api.__add__, np.add, [3, 1, 2], [3, 1, 1])
+        _run_test(singa_api.__add__, np.add, [2, 3, 4, 5], [5])
+        _run_test(singa_api.__add__, np.add, [2, 3, 4, 5], [1, 1, 1])
+        _run_test(singa_api.__add__, np.add, [2, 3, 4, 5], [1, 1, 1, 1])
+        _run_test(singa_api.__add__, np.add, [3, 1, 2, 1], [3, 1, 2])
+        _run_test(singa_api.__add__, np.add, [4, 5],
+                  [2, 3, 4, 5])  # 45+2345=2345
+        _run_test(singa_api.__add__, np.add, [2, 3, 4, 5],
+                  [4, 5])  # 45+2345=2345
+        _run_test(singa_api.__add__, np.add, [1, 4, 5],
+                  [2, 3, 1, 1])  # 145+2311=2345
+        _run_test(singa_api.__add__, np.add, [3, 4, 5],
+                  [2, 1, 1, 1])  # 345+2111=2345
 
-        _run_test(singa_api.__mul__,np.multiply,[2,3],[2,3])
-        _run_test(singa_api.__mul__,np.multiply,[6],[1])
-        _run_test(singa_api.__mul__,np.multiply,[3,2],[1])
-        _run_test(singa_api.__mul__,np.multiply,[3,1,2],[3,1,1])
-        _run_test(singa_api.__mul__,np.multiply,[2,3,4,5],[5])
-        _run_test(singa_api.__mul__,np.multiply,[2,3,4,5],[1,1,1])
-        _run_test(singa_api.__mul__,np.multiply,[2,3,4,5],[1,1,1,1])
-        _run_test(singa_api.__mul__,np.multiply,[3,1,2,1],[3,1,2])
-        _run_test(singa_api.__mul__,np.multiply,[4,5],[2,3,4,5]) # 45+2345=2345
-        _run_test(singa_api.__mul__,np.multiply,[2,3,4,5],[4,5]) # 45+2345=2345
-        _run_test(singa_api.__mul__,np.multiply,[1,4,5],[2,3,1,1]) # 145+2311=2345
-        _run_test(singa_api.__mul__,np.multiply,[3,4,5],[2,1,1,1]) # 345+2111=2345
+        _run_test(singa_api.__mul__, np.multiply, [2, 3], [2, 3])
+        _run_test(singa_api.__mul__, np.multiply, [6], [1])
+        _run_test(singa_api.__mul__, np.multiply, [3, 2], [1])
+        _run_test(singa_api.__mul__, np.multiply, [3, 1, 2], [3, 1, 1])
+        _run_test(singa_api.__mul__, np.multiply, [2, 3, 4, 5], [5])
+        _run_test(singa_api.__mul__, np.multiply, [2, 3, 4, 5], [1, 1, 1])
+        _run_test(singa_api.__mul__, np.multiply, [2, 3, 4, 5], [1, 1, 1, 1])
+        _run_test(singa_api.__mul__, np.multiply, [3, 1, 2, 1], [3, 1, 2])
+        _run_test(singa_api.__mul__, np.multiply, [4, 5],
+                  [2, 3, 4, 5])  # 45+2345=2345
+        _run_test(singa_api.__mul__, np.multiply, [2, 3, 4, 5],
+                  [4, 5])  # 45+2345=2345
+        _run_test(singa_api.__mul__, np.multiply, [1, 4, 5],
+                  [2, 3, 1, 1])  # 145+2311=2345
+        _run_test(singa_api.__mul__, np.multiply, [3, 4, 5],
+                  [2, 1, 1, 1])  # 345+2111=2345
 
     def test_transpose_and_mul(self):
-        s1 = [3,2,1,1]
-        s2 = [3,2,1,1]
+        s1 = [3, 2, 1, 1]
+        s2 = [3, 2, 1, 1]
         x_0 = np.random.random(s1).astype(np.float32)
         y_0 = np.random.random(s2).astype(np.float32)
 
         x0 = tensor.Tensor(device=gpu_dev, data=x_0)
         y0 = tensor.Tensor(device=gpu_dev, data=y_0)
-        x1 = x0.transpose([3,2,1,0])
+        x1 = x0.transpose([3, 2, 1, 0])
 
         #print(x1.shape)
         #print(y0.shape)
 
         z0 = x1 * y0
-        np.testing.assert_array_almost_equal(tensor.to_numpy(z0), x_0.transpose()*y_0 )
+        np.testing.assert_array_almost_equal(tensor.to_numpy(z0),
+                                             x_0.transpose() * y_0)
 
     def test_transpose_and_mul_2(self):
-        s1 = [1,5,1,3]
-        s2 = [3,1,1,4]
-        axis0=[3,2,1,0] # 3121
-        axis1=[1,0,2,3] # 1314
-        x_0 = np.random.random(s1).astype(np.float32)
-        y_0 = np.random.random(s2).astype(np.float32)
+        def _test(s1, s2, axis1, axis2, s3):
+            x_0 = np.random.random(s1).astype(np.float32)
+            y_0 = np.random.random(s2).astype(np.float32)
 
-        x0 = tensor.Tensor(device=gpu_dev, data=x_0)
-        y0 = tensor.Tensor(device=gpu_dev, data=y_0)
+            x0 = tensor.Tensor(device=gpu_dev, data=x_0)
+            y0 = tensor.Tensor(device=gpu_dev, data=y_0)
 
-        x1 = x0.transpose(axis0)
-        y1 = y0.transpose(axis1)
-        #print(x1.shape)
-        #print(y1.shape)
+            x1 = x0.transpose(axis1)
+            y1 = y0.transpose(axis2)
+            #print(x1.shape)
+            #print(y1.shape)
 
-        z0 = x1 * y1
-        np.testing.assert_array_almost_equal(tensor.to_numpy(z0), x_0.transpose(axis0)*y_0.transpose(axis1) )
-        np.testing.assert_array_almost_equal(z0.shape, [3,3,5,4])
+            z0 = x1 * y1
+            np.testing.assert_array_almost_equal(
+                tensor.to_numpy(z0),
+                x_0.transpose(axis1) * y_0.transpose(axis2))
+            np.testing.assert_array_almost_equal(z0.shape, s3)
 
+        s1 = [1, 5, 1, 3]
+        s2 = [3, 1, 1, 4]
+        axis1 = [3, 2, 1, 0]  # 3121
+        axis2 = [1, 0, 2, 3]  # 1314
+        s3 = [3, 3, 5, 4]
+        _test(s1, s2, axis1, axis2, s3)
 
+        s1 = [1, 5, 1]
+        s2 = [1, 3, 2]
+        axis1 = [2, 1, 0]  # 151
+        axis2 = [1, 0, 2]  # 312
+        s3 = [3, 5, 2]
+        _test(s1, s2, axis1, axis2, s3)
+
+        s1 = [5, 1]
+        s2 = [1, 3]
+        axis1 = [1, 0]  # 15
+        axis2 = [1, 0]  # 31
+        s3 = [3, 5]
+        _test(s1, s2, axis1, axis2, s3)
 
 
 if __name__ == '__main__':
