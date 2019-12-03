9041bfb9bf427034d6b1f22079c2386d594200be
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-12-03 13:12:27 +0800

    [site] reorg user to data/users.js

---
+++ website/data/users.js
@@ -0,0 +1,135 @@
+/*
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+*/
+
+module.exports = [
+  // Please contact SINGA team if you
+  // want to feature your usage of Apache SINGA
+  {
+    caption: 'NetEase',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/NetEase.jpg',
+    infoLink: 'http://tech.163.com/17/0602/17/CLUL016I00098GJ5.html',
+    pinned: true,
+  },
+  {
+    caption: 'yzBigData',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/yzBigData.png',
+    infoLink: 'http://www.yzbigdata.com/',
+    pinned: true,
+  },
+  {
+    caption: 'Shentilium',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/shentilium.png',
+    infoLink: 'https://shentilium.com/',
+    pinned: true,
+  },
+  {
+    caption: 'Foodlg',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/foodlg.png',
+    infoLink: 'http://www.foodlg.com/',
+    pinned: true,
+  },
+  {
+    caption: 'Medilot',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/medilot.svg',
+    infoLink: 'https://medilot.com/technologies',
+    pinned: true,
+  },
+  {
+    caption: 'MZH Tech',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/mzhtech.png',
+    infoLink: 'http://www.mzhtechnologies.com/index.html',
+    pinned: true,
+  },
+  {
+    caption: 'NUH SG',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/nuhlogo.jpg',
+    infoLink: 'https://www.nuh.com.sg/Pages/Home.aspx',
+    pinned: true,
+  },
+  {
+    caption: 'SGH SG',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/sgh.png',
+    infoLink: 'https://www.sgh.com.sg/',
+    pinned: true,
+  },
+  {
+    caption: 'TTSH SG',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/TTSH_Logo.gif',
+    infoLink: 'https://www.ttsh.com.sg/Pages/default.aspx',
+    pinned: true,
+  },
+  {
+    caption: 'JurongHealth SG',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/juronghealth.png',
+    infoLink: 'https://www.juronghealthcampus.com.sg/',
+    pinned: true,
+  },
+  {
+    caption: 'Carnegie Tech',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/carnegietech.png',
+    infoLink: 'https://www.carnegietechnologies.com/',
+    pinned: true,
+  },
+  {
+    caption: 'CBRE',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/CBRE.jpg',
+    infoLink: 'http://www.cbre.com',
+    pinned: true,
+  },
+  {
+    caption: 'Noblis',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/noblis.png',
+    infoLink: 'https://noblis.org/',
+    pinned: true,
+  },
+  {
+    caption: 'CitiGroup',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/citigroup-logo.jpg',
+    infoLink: 'https://www.citigroup.com/citi/',
+    pinned: true,
+  },
+];
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,3 +1,22 @@
+/*
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+*/
+
 /**
 Configuration file: The website/siteConfig.js file 
 is the main configuration file used by Docusaurus.
@@ -9,120 +28,7 @@ is the main configuration file used by Docusaurus.
 // List of projects/orgs using your project for the users page.
 // this field is used by example the pages/en/index.js 
 // and pages/en/users.js files provided
-const users = [
-  {
-    caption: 'NetEase',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/NetEase.jpg',
-    infoLink: 'http://tech.163.com/17/0602/17/CLUL016I00098GJ5.html',
-    pinned: true,
-  },
-  {
-    caption: 'yzBigData',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/yzBigData.png',
-    infoLink: 'http://www.yzbigdata.com/',
-    pinned: true,
-  },
-  {
-    caption: 'Shentilium',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/shentilium.png',
-    infoLink: 'https://shentilium.com/',
-    pinned: true,
-  },
-  {
-    caption: 'Foodlg',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/foodlg.png',
-    infoLink: 'http://www.foodlg.com/',
-    pinned: true,
-  },
-  {
-    caption: 'Medilot',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/medilot.svg',
-    infoLink: 'https://medilot.com/technologies',
-    pinned: true,
-  },
-  {
-    caption: 'MZH Tech',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/mzhtech.png',
-    infoLink: 'http://www.mzhtechnologies.com/index.html',
-    pinned: true,
-  },
-  {
-    caption: 'NUH SG',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/nuhlogo.jpg',
-    infoLink: 'https://www.nuh.com.sg/Pages/Home.aspx',
-    pinned: true,
-  },
-  {
-    caption: 'SGH SG',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/sgh.png',
-    infoLink: 'https://www.sgh.com.sg/',
-    pinned: true,
-  },
-  {
-    caption: 'TTSH SG',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/TTSH_Logo.gif',
-    infoLink: 'https://www.ttsh.com.sg/Pages/default.aspx',
-    pinned: true,
-  },
-  {
-    caption: 'JurongHealth SG',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/juronghealth.png',
-    infoLink: 'https://www.juronghealthcampus.com.sg/',
-    pinned: true,
-  },
-  {
-    caption: 'Carnegie Tech',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/carnegietech.png',
-    infoLink: 'https://www.carnegietechnologies.com/',
-    pinned: true,
-  },
-  {
-    caption: 'CBRE',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/CBRE.jpg',
-    infoLink: 'http://www.cbre.com',
-    pinned: true,
-  },
-  {
-    caption: 'Noblis',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/noblis.png',
-    infoLink: 'https://noblis.org/',
-    pinned: true,
-  },
-  {
-    caption: 'CitiGroup',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/citigroup-logo.jpg',
-    infoLink: 'https://www.citigroup.com/citi/',
-    pinned: true,
-  },
-];
+const users = require('./data/users')
 
 const repoUrl = 'https://github.com/apache/singa'
 
