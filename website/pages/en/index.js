f6165444d8b9ea224f3f45f2f404dfc8ddfb512e
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-12-03 22:26:08 +0800

    [site] model the index.js after docusaurus 1.x

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -1,206 +1,137 @@
-/**
-Pages: The website/pages directory
-contains example top-level pages for the site.
- */
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
+
+/*
+ Pages: The website/pages directory
+ contains example top-level pages for the site.
+ index.js is the landing page
+*/
 
 const React = require('react');
 
 const CompLibrary = require('../../core/CompLibrary.js');
 
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
+const Showcase = require(`${process.cwd()}/core/Showcase.js`);
+// TODO: add <translate> tags
+// const translate = require('../../server/translate.js').translate;
 
-class HomeSplash extends React.Component {
-  render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+function HomeSplash(props) {
+  const {siteConfig, language} = props;
 
-    const SplashContainer = props => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
+  return (
+    <div className="index-hero">
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: "none" }}
+        src={`${siteConfig.baseUrl}img/unsplash-ocean.jpg`}
+        alt="increase priority"
+      />
+      <div className="index-hero-inner">
+        <img
+          alt="SINGA-Ocean"
+          className="index-hero-logo"
+          src={`${siteConfig.baseUrl}img/singa.png`}
+        />
+        <h1 className="index-hero-project-tagline">
+          A Distributed Deep Learning Platform
+        </h1>
+        <div className="index-ctas">
+          <a
+            className="button index-ctas-get-started-button"
+            href={`${siteConfig.baseUrl}docs/${language}/installation`}>
+            Get Started
+          </a>
+          <span className="index-ctas-github-button">
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=apache&amp;repo=singa&amp;type=star&amp;count=true&amp;size=large"
+              frameBorder={0}
+              scrolling={0}
+              width={160}
+              height={30}
+              title="GitHub Stars"
+            />
+          </span>
         </div>
       </div>
-    );
-
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
-      </h2>
-    );
-
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
-    );
-
-    return (
-      <SplashContainer>
-        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
-        <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
-          <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection>
-        </div>
-      </SplashContainer>
-    );
-  }
+    </div>
+  );
 }
 
 class Index extends React.Component {
   render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
-
-    const Block = props => (
-      <Container
-        padding={['bottom', 'top']}
-        id={props.id}
-        background={props.background}>
-        <GridBlock
-          align="center"
-          contents={props.children}
-          layout={props.layout}
-        />
-      </Container>
-    );
-
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
-      </div>
-    );
-
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
-            image: `${baseUrl}img/undraw_code_review.svg`,
-            imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations',
-          },
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/undraw_note_list.svg`,
-            imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    );
-
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content:
-              'Each new Docusaurus project has **randomly-generated** theme colors.',
-            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
-            imageAlign: 'right',
-            title: 'Randomly Generated Theme Colors',
-          },
-        ]}
-      </Block>
-    );
-
-    const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/undraw_react.svg`,
-            imageAlign: 'top',
-            title: 'Feature One',
-          },
-          {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/undraw_operating_system.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two',
-          },
-        ]}
-      </Block>
-    );
-
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    };
+    const {config: siteConfig, language = 'en'} = this.props;
+    const pinnedUsersToShowcase = siteConfig.users.filter(user => user.pinned);
 
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="announcement">
+          <div className="announcement-inner">
+           {`Apache SINGA is an Apache Top Level Project, open source distributed training platform
+            for deep learning and machine learning models`}
+          </div>
+        </div>
         <div className="mainContainer">
-          <Features />
-          <FeatureCallout />
-          <LearnHow />
-          <TryOut />
-          <Description />
-          <Showcase />
+          <Container padding={['bottom', 'top']} background="light">
+            <GridBlock
+              align="center"
+              contents={[
+                {
+                  content: `Apache SINGA focused on [distributed](https://dl.acm.org/citation.cfm?doid=2733373.2807410)
+                   deep learning by partitioning the model and data onto nodes in a cluster and
+                    [parallelize](https://dl.acm.org/citation.cfm?doid=2733373.2806232) the training`,
+                  image: `${siteConfig.baseUrl}img/undraw_mind_map_cwng.svg`,
+                  imageAlign: 'top',
+                  imageAlt: 'Distributed Learning',
+                  title: 'Distributed Learning',
+                },
+                {
+                  content: `Apache SINGA v2.0.0 has AutoML features, a Healthcare
+                   [model zoo](${siteConfig.baseUrl}docs/${this.props.language}/model-zoo-char-rnn),
+                    and facility for porting other models onto SINGA`,
+                  image: `${siteConfig.baseUrl}img/undraw_web_development_w2vv.svg`,
+                  imageAlign: 'top',
+                  imageAlt: 'AutoML and Model Zoo',
+                  title: 'AutoML and Model Zoo',
+                },
+              ]}
+              layout="twoColumn"
+            />
+          </Container>
+          <div className="productShowcaseSection paddingBottom">
+            <h2 style={{ color: "#904600" }}>
+              Who is Using Apache SINGA?
+            </h2>
+            <p>
+              Apache SINGA powers many organizations and companies...
+            </p>
+            <Showcase users={pinnedUsersToShowcase} />
+            <div className="more-users">
+              <a
+                className="button"
+                href={`${siteConfig.baseUrl}${this.props.language}/users`}>
+                All Apache SINGA Users
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,29 +1,28 @@
 /*
-   Licensed to the Apache Software Foundation (ASF) under one
-   or more contributor license agreements.  See the NOTICE file
-   distributed with this work for additional information
-   regarding copyright ownership.  The ASF licenses this file
-   to you under the Apache License, Version 2.0 (the
-   "License"); you may not use this file except in compliance
-   with the License.  You may obtain a copy of the License at
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing,
-   software distributed under the License is distributed on an
-   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-   KIND, either express or implied.  See the License for the
-   specific language governing permissions and limitations
-   under the License.
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
 */
 
-/**
-Configuration file: The website/siteConfig.js file 
-is the main configuration file used by Docusaurus.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
+/*
+ Configuration file: The website/siteConfig.js file 
+ is the main configuration file used by Docusaurus.
+ See https://docusaurus.io/docs/site-config for all the possible
+ site configuration options.
+*/
 
 // List of projects/orgs using your project for the users page.
 // this field is used by example the pages/en/index.js 
@@ -74,8 +73,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    // primaryColor: "#1A2B34",
-    primaryColor: "#0058ae",
+    // many of the colors are over-written in custom.css
+    primaryColor: '#5c6976', // gray
     secondaryColor: "#808080" // 2nd layer of toolbar in smaller screen
   },
 
