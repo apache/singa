34e796cd6273a8e8403f20d0bca8f2dbcb247200
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-12-05 16:28:47 +0800

    [site] fix users.js style, add apache license to help.js

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -1,9 +1,21 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*/
 
 const React = require('react');
 
@@ -18,7 +30,8 @@ class Users extends React.Component {
       return null;
     }
 
-    const editUrl = `${siteConfig.repoUrl}/edit/master/website/siteConfig.js`;
+    // TODO: change the editURL once repo reorg is complete
+    const editUrl = `${siteConfig.repoUrl}/edit/master/website/data/users.js`;
     const showcase = siteConfig.users.map(user => (
       <a href={user.infoLink} key={user.infoLink}>
         <img src={user.image} alt={user.caption} title={user.caption} />
@@ -27,16 +40,17 @@ class Users extends React.Component {
 
     return (
       <div className="mainContainer">
-        <Container padding={['bottom', 'top']}>
+        <Container padding={['bottom']}>
           <div className="showcaseSection">
             <div className="prose">
-              <h1>Who is Using This?</h1>
-              <p>This project is used by many folks</p>
+              <h1>Users of Apache SINGA</h1>
+              <p>Apache SINGA is used by many projects,
+                 especially in healthcare and finance</p>
             </div>
             <div className="logos">{showcase}</div>
-            <p>Are you using this project?</p>
+            <p>Is your project using Apache SINGA?</p>
             <a href={editUrl} className="button">
-              Add your company
+              Add your logo with a Pull Request
             </a>
           </div>
         </Container>
@@ -44,5 +58,7 @@ class Users extends React.Component {
     );
   }
 }
+
+Users.title = 'Users';
 
 module.exports = Users;
