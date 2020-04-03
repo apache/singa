978db2653262743172c174c2b9fb17457140f929
xjdkc <xjdkcsq3@gmail.com>
2020-04-03 14:32:00 +0000

    add and pass all the unit tests of mudle

--- test/python/test_module.py
+++ test/python/test_module.py
@@ -90,18 +90,47 @@ class TestPythonModule(unittest.TestCase):
         self.B1 = tensor.to_numpy(model.b1)
 
     def numpy_forward(self, inputs):
-        res = np.matmul(inputs, self.W0)
-        res = np.add(res, self.B0)
-        res = np.maximum(res, 0)
-        res = np.matmul(res, self.W1)
-        res = np.add(res, self.B1)
-        return res
+        self.x1 = np.matmul(inputs, self.W0)
+        self.x2 = np.add(self.x1, self.B0)
+        self.x3 = np.maximum(self.x2, 0)
+        self.x4 = np.matmul(self.x3, self.W1)
+        self.x5 = np.add(self.x4, self.B1)
+        return self.x5
 
     def numpy_loss(self, out, y):
-        pass
+        exp_out = np.exp(out - np.max(out, axis=-1, keepdims=True))
+        self.softmax = exp_out / np.sum(exp_out, axis=-1, keepdims=True)
+
+        loss = np.sum(y * np.log(self.softmax)) / -self.softmax.shape[0]
+
+        return loss
 
     def numpy_optim(self, loss):
-        pass
+        # calculate gradients
+        label_sum = np.sum(self.label, axis=-1)
+        dloss = self.softmax - self.label / label_sum.reshape(
+            label_sum.shape[0], 1)
+        dloss /= self.softmax.shape[0]
+
+        dx5 = dloss
+        db1 = np.sum(dloss, 0)
+
+        dx4 = np.matmul(dx5, self.W1.T)
+        dw1 = np.matmul(self.x3.T, dx5)
+
+        dx3 = dx4 * (self.x3 > 0)
+
+        dx2 = dx3
+        db0 = np.sum(dx3, 0)
+
+        dx1 = np.matmul(dx2, self.W0.T)
+        dw0 = np.matmul(self.data.T, dx2)
+
+        # update all the params
+        self.W0 -= 0.05 * dw0
+        self.B0 -= 0.05 * db0
+        self.W1 -= 0.05 * dw1
+        self.B1 -= 0.05 * db1
 
     def setUp(self):
         self.sgd = opt.SGD(lr=0.05)
@@ -114,6 +143,7 @@ class TestPythonModule(unittest.TestCase):
     def test_forward(self):
         for dev in [cpu_dev, gpu_dev]:
             model = MLP(self.sgd)
+            model.train()
             model.on_device(dev)
             self.get_numpy_params(model)
 
@@ -126,6 +156,7 @@ class TestPythonModule(unittest.TestCase):
     def test_forward_loss(self):
         for dev in [cpu_dev, gpu_dev]:
             model = MLP(self.sgd)
+            model.train()
             model.on_device(dev)
             self.get_numpy_params(model)
 
@@ -136,21 +167,100 @@ class TestPythonModule(unittest.TestCase):
             np_loss = self.numpy_loss(np_out, self.label)
 
             np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(loss), np_loss)
 
     def test_forward_loss_optim(self):
-        pass
+        for dev in [cpu_dev, gpu_dev]:
+            model = MLP(self.sgd)
+            model.train()
+            model.on_device(dev)
+            self.get_numpy_params(model)
 
-    def test_without_graph(self):
-        pass
+            out = model(self.inputs)
+            loss = model.loss(out, self.target)
+            model.optim(loss)
+
+            np_out = self.numpy_forward(self.data)
+            np_loss = self.numpy_loss(np_out, self.label)
+            self.numpy_optim(np_loss)
+
+            np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(loss), np_loss)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.w0),
+                                                 self.W0)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.b0),
+                                                 self.B0)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.w1),
+                                                 self.W1)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.b1),
+                                                 self.B1)
+
+    def test_train_without_graph(self):
+        for dev in [cpu_dev, gpu_dev]:
+            model = MLP(self.sgd)
+            model.train()
+            model.on_device(dev)
+            model.graph(False)
+            self.get_numpy_params(model)
+
+            out = model(self.inputs)
+            loss = model.loss(out, self.target)
+            model.optim(loss)
+
+            np_out = self.numpy_forward(self.data)
+            np_loss = self.numpy_loss(np_out, self.label)
+            self.numpy_optim(np_loss)
+
+            np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(loss), np_loss)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.w0),
+                                                 self.W0)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.b0),
+                                                 self.B0)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.w1),
+                                                 self.W1)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.b1),
+                                                 self.B1)
 
     def test_run_in_serial(self):
-        pass
+        for dev in [cpu_dev, gpu_dev]:
+            model = MLP(self.sgd)
+            model.train()
+            model.on_device(dev)
+            model.graph(True, False)
+            self.get_numpy_params(model)
 
-    def test_train(self):
-        pass
+            out = model(self.inputs)
+            loss = model.loss(out, self.target)
+            model.optim(loss)
 
-    def test_validate(self):
-        pass
+            np_out = self.numpy_forward(self.data)
+            np_loss = self.numpy_loss(np_out, self.label)
+            self.numpy_optim(np_loss)
+
+            np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(loss), np_loss)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.w0),
+                                                 self.W0)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.b0),
+                                                 self.B0)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.w1),
+                                                 self.W1)
+            np.testing.assert_array_almost_equal(tensor.to_numpy(model.b1),
+                                                 self.B1)
+
+    def test_evaluate(self):
+        for dev in [cpu_dev, gpu_dev]:
+            model = MLP(self.sgd)
+            model.eval()
+            model.on_device(dev)
+            self.get_numpy_params(model)
+
+            out = model(self.inputs)
+
+            np_out = self.numpy_forward(self.data)
+
+            np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
 
 
 if __name__ == '__main__':
