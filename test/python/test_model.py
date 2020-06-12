2857ec74813d53eab4584f2e394fdff387fa1711
xjdkc <xjdkcsq3@gmail.com>
2020-06-12 22:07:14 +0000

    move the test cases in test_module to test_model

--- test/python/test_model.py
+++ test/python/test_model.py
@@ -24,6 +24,7 @@ import unittest
 import numpy as np
 
 from singa import singa_wrap as singa_api
+from singa.tensor import Tensor
 from singa import autograd
 from singa import tensor
 from singa import device
@@ -74,6 +75,34 @@ class MyModel(model.Model):
 
     def optim(self, loss):
         self.optimizer(loss)
+
+
+class MLP(model.Model):
+
+    def __init__(self, data_size=10, perceptron_size=100, num_classes=10):
+        super(MLP, self).__init__()
+        self.num_classes = num_classes
+        self.dimension = 2
+
+        self.relu = layer.ReLU()
+        self.linear1 = layer.Linear(perceptron_size)
+        self.linear2 = layer.Linear(num_classes)
+        self.softmax_cross_entropy = layer.SoftMaxCrossEntropy()
+
+    def forward(self, inputs):
+        y = self.linear1(inputs)
+        y = self.relu(y)
+        y = self.linear2(y)
+        return y
+
+    def train_one_batch(self, x, y):
+        out = self.forward(x)
+        loss = self.softmax_cross_entropy(out, y)
+        self.optimizer(loss)
+        return out, loss
+
+    def set_optimizer(self, optimizer):
+        self.optimizer = optimizer
 
 
 # lstm testing
@@ -280,6 +309,169 @@ class TestModelSaveMethods(unittest.TestCase):
     def test_save_states_load_states_cpu(self):
         self._save_states_load_states_helper(cpu_dev, graph_flag=False)
         self._save_states_load_states_helper(cpu_dev, graph_flag=True)
+
+
+class TestPythonModule(unittest.TestCase):
+
+    def to_categorical(self, y, num_classes):
+        y = np.array(y, dtype="int")
+        n = y.shape[0]
+        categorical = np.zeros((n, num_classes))
+        categorical[np.arange(n), y] = 1
+        return categorical
+
+    def generate_data(self, dev, num=400):
+        f = lambda x: (5 * x + 1)
+
+        x = np.random.uniform(-1, 1, num)
+        y = f(x) + 2 * np.random.randn(len(x))
+
+        self.label = np.asarray([5 * a + 1 > b for (a, b) in zip(x, y)])
+        self.data = np.array([[a, b] for (a, b) in zip(x, y)], dtype=np.float32)
+        self.label = self.to_categorical(self.label, 2).astype(np.float32)
+
+        self.inputs = Tensor(data=self.data, device=dev)
+        self.target = Tensor(data=self.label, device=dev)
+
+    def get_params(self, model):
+        params = model.get_params()
+        self.w0 = params['linear1.W']
+        self.b0 = params['linear1.b']
+        self.w1 = params['linear2.W']
+        self.b1 = params['linear2.b']
+
+        self.W0 = tensor.to_numpy(self.w0)
+        self.B0 = tensor.to_numpy(self.b0)
+        self.W1 = tensor.to_numpy(self.w1)
+        self.B1 = tensor.to_numpy(self.b1)
+
+    def numpy_forward(self, inputs):
+        self.x1 = np.matmul(inputs, self.W0)
+        self.x2 = np.add(self.x1, self.B0)
+        self.x3 = np.maximum(self.x2, 0)
+        self.x4 = np.matmul(self.x3, self.W1)
+        self.x5 = np.add(self.x4, self.B1)
+        return self.x5
+
+    def numpy_train_one_batch(self, inputs, y):
+        # forward propagation
+        out = self.numpy_forward(inputs)
+
+        # softmax cross entropy loss
+        exp_out = np.exp(out - np.max(out, axis=-1, keepdims=True))
+        self.softmax = exp_out / np.sum(exp_out, axis=-1, keepdims=True)
+        loss = np.sum(y * np.log(self.softmax)) / -self.softmax.shape[0]
+
+        # optimize
+        # calculate gradients
+        label_sum = np.sum(self.label, axis=-1)
+        dloss = self.softmax - self.label / label_sum.reshape(
+            label_sum.shape[0], 1)
+        dloss /= self.softmax.shape[0]
+
+        dx5 = dloss
+        db1 = np.sum(dloss, 0)
+
+        dx4 = np.matmul(dx5, self.W1.T)
+        dw1 = np.matmul(self.x3.T, dx5)
+
+        dx3 = dx4 * (self.x3 > 0)
+
+        dx2 = dx3
+        db0 = np.sum(dx3, 0)
+
+        dx1 = np.matmul(dx2, self.W0.T)
+        dw0 = np.matmul(self.data.T, dx2)
+
+        # update all the params
+        self.W0 -= 0.05 * dw0
+        self.B0 -= 0.05 * db0
+        self.W1 -= 0.05 * dw1
+        self.B1 -= 0.05 * db1
+        return out, loss
+
+    def setUp(self):
+        self.sgd = opt.SGD(lr=0.05)
+
+        cpu_dev.ResetGraph()
+        if singa_api.USE_CUDA:
+            gpu_dev.ResetGraph()
+
+    def tearDown(self):
+        cpu_dev.ResetGraph()
+        if singa_api.USE_CUDA:
+            gpu_dev.ResetGraph()
+
+    def _forward_helper(self, dev, is_train, use_graph, sequential):
+        self.generate_data(dev)
+        model = MLP(self.sgd)
+        model.compile([self.inputs],
+                      is_train=is_train,
+                      use_graph=use_graph,
+                      sequential=sequential)
+
+        self.get_params(model)
+
+        out = model(self.inputs)
+        np_out = self.numpy_forward(self.data)
+
+        np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
+
+    def _train_one_batch_helper(self, dev, is_train, use_graph, sequential):
+        self.generate_data(dev)
+        model = MLP(num_classes=2)
+        model.set_optimizer(self.sgd)
+        model.compile([self.inputs],
+                      is_train=is_train,
+                      use_graph=use_graph,
+                      sequential=sequential)
+
+        self.get_params(model)
+
+        out, loss = model(self.inputs, self.target)
+        np_out, np_loss = self.numpy_train_one_batch(self.data, self.label)
+
+        np.testing.assert_array_almost_equal(tensor.to_numpy(out), np_out)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(loss), np_loss)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(self.w0), self.W0)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(self.b0), self.B0)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(self.w1), self.W1)
+        np.testing.assert_array_almost_equal(tensor.to_numpy(self.b1), self.B1)
+
+    def test_forward_cpu(self):
+        self._forward_helper(cpu_dev, False, True, False)
+
+    @unittest.skipIf(not singa_api.USE_CUDA, 'CUDA is not enabled')
+    def test_forward_gpu(self):
+        self._forward_helper(gpu_dev, False, True, False)
+
+    def test_evaluate_cpu(self):
+        self._forward_helper(cpu_dev, False, False, False)
+
+    @unittest.skipIf(not singa_api.USE_CUDA, 'CUDA is not enabled')
+    def test_evaluate_gpu(self):
+        self._forward_helper(gpu_dev, False, False, False)
+
+    def test_train_one_batch_cpu(self):
+        self._train_one_batch_helper(cpu_dev, True, True, False)
+
+    @unittest.skipIf(not singa_api.USE_CUDA, 'CUDA is not enabled')
+    def test_train_one_batch_gpu(self):
+        self._train_one_batch_helper(gpu_dev, True, True, False)
+
+    def test_without_graph_cpu(self):
+        self._train_one_batch_helper(cpu_dev, True, False, False)
+
+    @unittest.skipIf(not singa_api.USE_CUDA, 'CUDA is not enabled')
+    def test_without_graph_gpu(self):
+        self._train_one_batch_helper(gpu_dev, True, False, False)
+
+    def test_run_in_serial_cpu(self):
+        self._train_one_batch_helper(cpu_dev, True, True, True)
+
+    @unittest.skipIf(not singa_api.USE_CUDA, 'CUDA is not enabled')
+    def test_run_in_serial_gpu(self):
+        self._train_one_batch_helper(gpu_dev, True, True, True)
 
 
 if __name__ == '__main__':
