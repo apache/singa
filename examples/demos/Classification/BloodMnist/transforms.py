3f7fb8245444d623427645ccf5a64688c811e868
wenqiao zhang <wenqiaozhang@zju.edu.cn>
2022-02-25 11:23:07 +0800

    update comments for class Compose

--- examples/demos/Classification/BloodMnist/transforms.py
+++ examples/demos/Classification/BloodMnist/transforms.py
@@ -26,33 +26,6 @@ import numbers
 
 
 class Compose:
-    """Composes several transforms together. This transform does not support torchscript.
-    Please, see the note below.
-
-    Args:
-        transforms (list of ``Transform`` objects): list of transforms to compose.
-
-    Example:
-        >>> transforms.Compose([
-        >>>     transforms.CenterCrop(10),
-        >>>     transforms.PILToTensor(),
-        >>>     transforms.ConvertImageDtype(torch.float),
-        >>> ])
-
-    .. note::
-        In order to script the transformations, please use ``torch.nn.Sequential`` as below.
-
-        >>> transforms = torch.nn.Sequential(
-        >>>     transforms.CenterCrop(10),
-        >>>     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        >>> )
-        >>> scripted_transforms = torch.jit.script(transforms)
-
-        Make sure to use only scriptable transformations, i.e. that work with ``torch.Tensor``, does not require
-        `lambda` functions or ``PIL.Image``.
-
-    """
-
     def __init__(self, transforms):
         self.transforms = transforms
 
