daa9c173d1b2720a07d34148745bee6f3376fa0d
zhaojing <zhaojing@comp.nus.edu.sg>
2023-10-25 10:50:22 +0800

    Update license headers for v4.1.0

--- examples/model_selection/TRAILS-Database-Native-Model-Selection/internal/ml/model_selection/src/third_pkg/models/shape_infers/__init__.py
+++
@@ -1,9 +0,0 @@
-#####################################################
-# Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2019.01 #
-#####################################################
-from .InferCifarResNet_width import InferWidthCifarResNet
-from .InferImagenetResNet import InferImagenetResNet
-from .InferCifarResNet_depth import InferDepthCifarResNet
-from .InferCifarResNet import InferCifarResNet
-from .InferMobileNetV2 import InferMobileNetV2
-from .InferTinyCellNet import DynamicShapeTinyNet
