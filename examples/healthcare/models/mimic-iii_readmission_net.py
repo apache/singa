be9594a29e5ef0fc2277d5ae26917e4550be139d
L0FX <111472480+L0FX@users.noreply.github.com>
2025-01-06 15:47:38 +0800

    Update mimic-iii_readmission_net.py

--- examples/healthcare/models/mimic-iii_readmission_net.py
+++ examples/healthcare/models/mimic-iii_readmission_net.py
@@ -17,303 +17,131 @@
 # under the License.
 #
 
-from singa import singa_wrap as singa
-from singa import device
+from singa import layer
+from singa import model
 from singa import tensor
 from singa import opt
-import numpy as np
-import time
+from singa import device
 import argparse
-from PIL import Image
+import numpy as np
 
 np_dtype = {"float16": np.float16, "float32": np.float32}
 
 singa_dtype = {"float16": tensor.float16, "float32": tensor.float32}
 
 
-# Data augmentation
-def augmentation(x, batch_size):
-    xpad = np.pad(x, [[0, 0], [0, 0], [4, 4], [4, 4]], 'symmetric')
-    for data_num in range(0, batch_size):
-        offset = np.random.randint(8, size=2)
-        x[data_num, :, :, :] = xpad[data_num, :,
-                                    offset[0]:offset[0] + x.shape[2],
-                                    offset[1]:offset[1] + x.shape[2]]
-        if_flip = np.random.randint(2)
-        if (if_flip):
-            x[data_num, :, :, :] = x[data_num, :, :, ::-1]
-    return x
+class MLP(model.Model):
+
+    def __init__(self, data_size=10, perceptron_size=100, num_classes=10):
+        super(MLP, self).__init__()
+        self.num_classes = num_classes
+        self.dimension = 2
+
+        self.relu = layer.ReLU()
+        self.linear1 = layer.Linear(perceptron_size)
+        self.linear2 = layer.Linear(num_classes)
+        self.softmax_cross_entropy = layer.SoftMaxCrossEntropy()
+
+    def forward(self, inputs):
+        y = self.linear1(inputs)
+        y = self.relu(y)
+        y = self.linear2(y)
+        return y
+
+    def train_one_batch(self, x, y, dist_option, spars):
+        out = self.forward(x)
+        loss = self.softmax_cross_entropy(out, y)
+
+        if dist_option == 'plain':
+            self.optimizer(loss)
+        elif dist_option == 'half':
+            self.optimizer.backward_and_update_half(loss)
+        elif dist_option == 'partialUpdate':
+            self.optimizer.backward_and_partial_update(loss)
+        elif dist_option == 'sparseTopK':
+            self.optimizer.backward_and_sparse_update(loss,
+                                                      topK=True,
+                                                      spars=spars)
+        elif dist_option == 'sparseThreshold':
+            self.optimizer.backward_and_sparse_update(loss,
+                                                      topK=False,
+                                                      spars=spars)
+        return out, loss
+
+    def set_optimizer(self, optimizer):
+        self.optimizer = optimizer
 
 
-# Calculate accuracy
-def accuracy(pred, target):
-    # y is network output to be compared with ground truth (int)
-    y = np.argmax(pred, axis=1)
-    #print('y:',y)
-    #print('tar:',target)    
-    a = y == target
-    #print(np.array(a, "int"))
-    correct = np.array(a, "int").sum()
-    return correct
+def create_model(pretrained=False, **kwargs):
+    """Constructs a CNN model.
+
+    Args:
+        pretrained (bool): If True, returns a pre-trained model.
+    
+    Returns:
+        The created CNN model.
+    """
+    model = MLP(**kwargs)
+
+    return model
 
 
-# Data partition according to the rank
-def partition(global_rank, world_size, train_x, train_y, val_x, val_y):
-    # Partition training data
-    data_per_rank = train_x.shape[0] // world_size
-    idx_start = global_rank * data_per_rank
-    idx_end = (global_rank + 1) * data_per_rank
-    train_x = train_x[idx_start:idx_end]
-    train_y = train_y[idx_start:idx_end]
+__all__ = ['MLP', 'create_model']
 
-    # Partition evaluation data
-    data_per_rank = val_x.shape[0] // world_size
-    idx_start = global_rank * data_per_rank
-    idx_end = (global_rank + 1) * data_per_rank
-    val_x = val_x[idx_start:idx_end]
-    val_y = val_y[idx_start:idx_end]
-    return train_x, train_y, val_x, val_y
-
-
-# Function to all reduce NUMPY accuracy and loss from multiple devices
-def reduce_variable(variable, dist_opt, reducer):
-    reducer.copy_from_numpy(variable)
-    dist_opt.all_reduce(reducer.data)
-    dist_opt.wait()
-    output = tensor.to_numpy(reducer)
-    return output
-
-
-def resize_dataset(x, image_size):
-    num_data = x.shape[0]
-    dim = x.shape[1]
-    X = np.zeros(shape=(num_data, dim, image_size, image_size),
-                 dtype=np.float32)
-    for n in range(0, num_data):
-        for d in range(0, dim):
-            X[n, d, :, :] = np.array(Image.fromarray(x[n, d, :, :]).resize(
-                (image_size, image_size), Image.BILINEAR),
-                                     dtype=np.float32)
-    return X
-
-
-def run(global_rank,
-        world_size,
-        local_rank,
-        max_epoch,
-        batch_size,
-        model,
-        data,
-        sgd,
-        graph,
-        verbosity,
-        dist_option='plain',
-        spars=None,
-        precision='float32'):
-    #dev = device.create_cuda_gpu_on(local_rank)  # need to change to CPU device for CPU-only machines
-    dev = device.get_default_device()
-    dev.SetRandSeed(0)
+if __name__ == "__main__":
     np.random.seed(0)
 
-    if data == 'mimic-iii':
-        from data import mimic_iii
-        train_x, train_y, val_x, val_y = mimic_iii.load()
-    else:
-       print('Wrong Dataset!')
-       sys.exit(0)
-
-
-    num_channels = train_x.shape[1]
-    image_size = train_x.shape[2]
-    data_size = np.prod(train_x.shape[1:train_x.ndim]).item()
-    num_classes = (np.max(train_y) + 1).item()
-    print(num_channels,image_size)
-
-
-    if model == 'mlp':
-        import os, sys, inspect
-        current = os.path.dirname(
-            os.path.abspath(inspect.getfile(inspect.currentframe())))
-        parent = os.path.dirname(current)
-        sys.path.insert(0, parent)
-        from mlp import model
-        model = model.create_model(data_size=data_size,
-                                    num_classes=num_classes)
-    else:
-        print('Wrong model!')
-        sys.exit(0)    
-
-    # For distributed training, sequential has better performance
-    if hasattr(sgd, "communicator"):
-        DIST = True
-        sequential = True
-    else:
-        DIST = False
-        sequential = False
-
-    if DIST:
-        train_x, train_y, val_x, val_y = partition(global_rank, world_size,
-                                                   train_x, train_y, val_x,
-                                                   val_y)
-
-    if model.dimension == 4:
-        tx = tensor.Tensor(
-            (batch_size, num_channels, model.input_size, model.input_size), dev,
-            singa_dtype[precision])
-    elif model.dimension == 2:
-        tx = tensor.Tensor((batch_size, data_size), dev, singa_dtype[precision])
-        np.reshape(train_x, (train_x.shape[0], -1))
-        np.reshape(val_x, (val_x.shape[0], -1))
-
-    ty = tensor.Tensor((batch_size,), dev, tensor.int32)
-    num_train_batch = train_x.shape[0] // batch_size
-    num_val_batch = val_x.shape[0] // batch_size
-    idx = np.arange(train_x.shape[0], dtype=np.int32)
-
-    # Attach model to graph
-    model.set_optimizer(sgd)
-    model.compile([tx], is_train=True, use_graph=graph, sequential=sequential)
-    dev.SetVerbosity(verbosity)
-
-    # Training and evaluation loop
-    for epoch in range(max_epoch):
-        start_time = time.time()
-        np.random.shuffle(idx)
-
-        if global_rank == 0:
-            print('Starting Epoch %d:' % (epoch))
-
-        # Training phase
-        train_correct = np.zeros(shape=[1], dtype=np.float32)
-        test_correct = np.zeros(shape=[1], dtype=np.float32)
-        train_loss = np.zeros(shape=[1], dtype=np.float32)
-
-        model.train()
-        for b in range(num_train_batch):
-            # if b % 100 == 0:
-            #     print ("b: \n", b)
-            # Generate the patch data in this iteration
-            x = train_x[idx[b * batch_size:(b + 1) * batch_size]]
-            if model.dimension == 4:
-                x = augmentation(x, batch_size)
-                if (image_size != model.input_size):
-                    x = resize_dataset(x, model.input_size)
-            x = x.astype(np_dtype[precision])
-            y = train_y[idx[b * batch_size:(b + 1) * batch_size]]
-
-            # Copy the patch data into input tensors
-            tx.copy_from_numpy(x)
-            ty.copy_from_numpy(y)
-
-            # Train the model
-            out, loss = model(tx, ty, dist_option, spars)
-            train_correct += accuracy(tensor.to_numpy(out), y)
-            train_loss += tensor.to_numpy(loss)[0]
-
-        if DIST:
-            # Reduce the evaluation accuracy and loss from multiple devices
-            reducer = tensor.Tensor((1,), dev, tensor.float32)
-            train_correct = reduce_variable(train_correct, sgd, reducer)
-            train_loss = reduce_variable(train_loss, sgd, reducer)
-
-        if global_rank == 0:
-            print('Training loss = %f, training accuracy = %f' %
-                  (train_loss, train_correct /
-                   (num_train_batch * batch_size * world_size)),
-                  flush=True)
-
-        # Evaluation phase
-        model.eval()
-        for b in range(num_val_batch):
-            x = val_x[b * batch_size:(b + 1) * batch_size]
-            if model.dimension == 4:
-                if (image_size != model.input_size):
-                    x = resize_dataset(x, model.input_size)
-            x = x.astype(np_dtype[precision])
-            y = val_y[b * batch_size:(b + 1) * batch_size]
-            tx.copy_from_numpy(x)
-            ty.copy_from_numpy(y)
-            out_test = model(tx)
-            test_correct += accuracy(tensor.to_numpy(out_test), y)
-
-        if DIST:
-            # Reduce the evaulation accuracy from multiple devices
-            test_correct = reduce_variable(test_correct, sgd, reducer)
-
-        # Output the evaluation accuracy
-        if global_rank == 0:
-            print('Evaluation accuracy = %f, Elapsed Time = %fs' %
-                  (test_correct / (num_val_batch * batch_size * world_size),
-                   time.time() - start_time),
-                  flush=True)
-
-    dev.PrintTimeProfiling()
-
-
-if __name__ == '__main__':
-    # Use argparse to get command config: max_epoch, model, data, etc., for single gpu training
-    parser = argparse.ArgumentParser(
-        description='Training using the autograd and graph.')
-    parser.add_argument(
-        'model',
-        choices=['cnn', 'resnet', 'xceptionnet', 'mlp', 'alexnet'],
-        default='cnn')
-    parser.add_argument('data',
-                        choices=['mnist', 'cifar10', 'cifar100','mimic-iii'],
-                        default='mnist')
+    parser = argparse.ArgumentParser()
     parser.add_argument('-p',
                         choices=['float32', 'float16'],
                         default='float32',
                         dest='precision')
-    parser.add_argument('-m',
-                        '--max-epoch',
-                        default=20,
-                        type=int,
-                        help='maximum epochs',
-                        dest='max_epoch')
-    parser.add_argument('-b',
-                        '--batch-size',
-                        default=64,
-                        type=int,
-                        help='batch size',
-                        dest='batch_size')
-    parser.add_argument('-l',
-                        '--learning-rate',
-                        default=0.005,
-                        type=float,
-                        help='initial learning rate',
-                        dest='lr')
-    # Determine which gpu to use
-    parser.add_argument('-i',
-                        '--device-id',
-                        default=0,
-                        type=int,
-                        help='which GPU to use',
-                        dest='device_id')
     parser.add_argument('-g',
                         '--disable-graph',
                         default='True',
                         action='store_false',
                         help='disable graph',
                         dest='graph')
-    parser.add_argument('-v',
-                        '--log-verbosity',
-                        default=0,
+    parser.add_argument('-m',
+                        '--max-epoch',
+                        default=1001,
                         type=int,
-                        help='logging verbosity',
-                        dest='verbosity')
-
+                        help='maximum epochs',
+                        dest='max_epoch')
     args = parser.parse_args()
 
-    sgd = opt.SGD(lr=args.lr, momentum=0.9, weight_decay=1e-5, dtype=singa_dtype[args.precision])
-    run(0,
-        1,
-        args.device_id,
-        args.max_epoch,
-        args.batch_size,
-        args.model,
-        args.data,
-        sgd,
-        args.graph,
-        args.verbosity,
-        precision=args.precision)
+    # generate the boundary
+    f = lambda x: (5 * x + 1)
+    bd_x = np.linspace(-1.0, 1, 200)
+    bd_y = f(bd_x)
+
+    # generate the training data
+    x = np.random.uniform(-1, 1, 400)
+    y = f(x) + 2 * np.random.randn(len(x))
+
+    # choose one precision
+    precision = singa_dtype[args.precision]
+    np_precision = np_dtype[args.precision]
+
+    # convert training data to 2d space
+    label = np.asarray([5 * a + 1 > b for (a, b) in zip(x, y)]).astype(np.int32)
+    data = np.array([[a, b] for (a, b) in zip(x, y)], dtype=np_precision)
+
+    dev = device.create_cuda_gpu_on(0)
+    sgd = opt.SGD(0.1, 0.9, 1e-5, dtype=singa_dtype[args.precision])
+    tx = tensor.Tensor((400, 2), dev, precision)
+    ty = tensor.Tensor((400,), dev, tensor.int32)
+    model = MLP(data_size=2, perceptron_size=3, num_classes=2)
+
+    # attach model to graph
+    model.set_optimizer(sgd)
+    model.compile([tx], is_train=True, use_graph=args.graph, sequential=True)
+    model.train()
+
+    for i in range(args.max_epoch):
+        tx.copy_from_numpy(data)
+        ty.copy_from_numpy(label)
+        out, loss = model(tx, ty, 'fp32', spars=None)
+
+        if i % 100 == 0:
+            print("training loss = ", tensor.to_numpy(loss)[0])
